9b775ab1aea1b217c2b18e44363aea1463c6bb4c
Yamada Koki <lenny.srv.826@gmail.com>
2020-04-16 17:47:46 +0900

    Add:  ログアウト機能追加 - ヘルパーメソッドはセッションヘルパーに書いたほうが良い？？？

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,19 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: session_params[:email])
 
-        if user&.autenticate(session_params[:password])
-            session[:user_id] = user_id
-            redirect_to root_url, notice: 'ログインしました。'
+        if user&.authenticate(session_params[:password])
+            session[:user_id] = user.id
+            flash[:success] = 'ログインしました。'
+            redirect_to root_url
         else
             render :new
         end
+    end
+
+    def destroy
+        reset_session
+        flash[:success] = 'ログアウトしました。'
+        redirect_to root_url
     end
 
     private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
 
     if @user.save
         flash[:success] = 'ユーザを登録しました。'
-        redirect_to @user
-        # redirect_to :root
+        # redirect_to @user
+        redirect_to :root
     else
         flash.now[:danger] = 'ユーザの登録に失敗しました。'
         render :new
